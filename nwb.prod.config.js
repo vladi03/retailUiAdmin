f3b242880052f55ce28e384787a6e9350664fab2
vladi <vladi03@hotmail.com>
2020-08-09 22:32:46 -0400

    action params 2

--- nwb.prod.config.js
+++ nwb.prod.config.js
@@ -6,6 +6,11 @@ module.exports = {
         copy: [
             // Copy directory contents to output
             {from: './src/retail-shop-flat-icon_small.jpg'}
-        ]
+        ],
+        define: {
+            'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+            'process.env.AUTH_CLIENT_ID': JSON.stringify('Aa7xpZd1nciCoIStp8BYJZO2Oix8WH2R'),
+            'process.env.AUTH_AUDIENCE': JSON.stringify('https://eventnod.netware.io')
+        }
     }
 };
