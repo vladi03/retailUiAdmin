0c5fc342bea06c271ea81234905daf2e56716ac6
vladi <vladi03@hotmail.com>
2020-08-15 00:16:44 -0400

    edit catalog : close button

--- src/desktop/catalog/CatalogTable.js
+++ src/desktop/catalog/CatalogTable.js
@@ -3,6 +3,8 @@ import {catalogModel} from "../../models/home/catalogModel";
 import {connectArray} from "../../utility/helpers";
 import {CatalogCard} from "./CatalogCard";
 import {makeStyles} from "@material-ui/core/styles";
+import {IconButton} from "@material-ui/core";
+import {Close} from "@material-ui/icons";
 
 export const CatalogTableComponent = ({catalogList,catalogListFiltered,
                               catalogListInit, onCatalogListInit}) => {
@@ -15,6 +17,8 @@ export const CatalogTableComponent = ({catalogList,catalogListFiltered,
     return (
         <Fragment>
             Test One, {catalogList.length}
+        <div className={classes.mainContainer}>
+
             <div className={classes.container}>
                 {catalogListFiltered.map((catalog, index)=>(
                     <CatalogCard
@@ -24,6 +28,17 @@ export const CatalogTableComponent = ({catalogList,catalogListFiltered,
                     />
                 ))}
             </div>
+            {inEdit &&
+            <div className={classes.containerEdit}>
+                <IconButton
+                    onClick={()=> setInEdit(false)}
+                >
+                    <Close/>
+                </IconButton>
+            </div>
+            }
+
+        </div>
         </Fragment>
     )
 };
@@ -35,6 +50,17 @@ const useStyle = makeStyles({
        display:"flex",
        flexWrap:"wrap",
        justifyContent: "space-between",
-       width: props => props.inEdit ? "50%" : "100%"
-   }
+       width: props => props.inEdit ? "45%" : "100%"
+   },
+    containerEdit: {
+        display:"flex",
+        flexWrap:"wrap",
+        justifyContent: "flex-end",
+        height: 80,
+        width: "45%"
+    },
+    mainContainer: {
+        display:"flex",
+        flexWrap:"wrap",
+    }
 });
