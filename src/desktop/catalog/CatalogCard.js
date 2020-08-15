0900c83508889f95fb24927a7aa6a57e549cbb88
vladi <vladi03@hotmail.com>
2020-08-14 22:59:38 -0400

    fix screen resize custom hooks

--- src/desktop/catalog/CatalogCard.js
+++ src/desktop/catalog/CatalogCard.js
@@ -1,15 +1,19 @@
 import React from "react";
 import {Card, CardMedia, CardHeader} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
+import {useCardSize} from "../../utility/useIsMobile";
 
 export const CatalogCard = ({catalog}) => {
+    /*
     const contWidth = window.innerWidth;
     let widthCalc = "";
     if(contWidth < 550) widthCalc= "calc(100vw - 5px)";
     else if(contWidth < 700) widthCalc= "calc(48vw - 5px)";
     else if(contWidth < 1200) widthCalc= "calc(32vw - 20px)";
     else widthCalc= "calc(24vw - 10px)";
-
+    */
+    const widthCalc = useCardSize();
+    console.log(widthCalc);
     const classes = useStyle({
         widthCalc
     });
--- src/utility/useIsMobile.js
+++ src/utility/useIsMobile.js
@@ -11,7 +11,37 @@ export const useIsMobile = () => {
         return () => {
             window.removeEventListener("resize", resize);
         };
-    });
+    },[]);
 
     return isMobile;
 };
+
+export function useCardSize() {
+    const calcCardSize = (contWidth) => {
+
+        let widthCalc = "";
+        if(contWidth < 550) widthCalc= "calc(100vw - 5px)";
+        else if(contWidth < 700) widthCalc= "calc(48vw - 5px)";
+        else if(contWidth < 1200) widthCalc= "calc(32vw - 20px)";
+        else widthCalc= "calc(24vw - 10px)";
+        console.log(contWidth);
+        console.log(widthCalc);
+        return widthCalc;
+    };
+    const [cardWidth, setCardWidth] = useState(calcCardSize(window.innerWidth));
+
+    useEffect(() => {
+        function resizeWidth() {
+            const res = calcCardSize(window.innerWidth);
+            setCardWidth(res);
+        }
+
+        window.addEventListener("resize", resizeWidth);
+
+        return () => {
+            window.removeEventListener("resize", resizeWidth);
+        };
+    },[]); // Empty array ensures that effect is only run on mount
+
+    return cardWidth;
+}
