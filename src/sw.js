2aedbd556bb59178a39e31dbddbda9b28a392b3a
vladi <vladi03@hotmail.com>
2021-03-06 23:41:45 -0500

    fix service worker

--- src/sw.js
+++ src/sw.js
@@ -6,7 +6,7 @@ const matchImageFunction = ({url, request, event}) => {
 };
 workbox.routing.registerRoute(
     matchImageFunction,///(\/#)|(\/$)/,
-    workbox.strategies.CacheFirst({
+    workbox.strategies.cacheFirst({
         cacheName: 'CatalogImages',
         plugins: [
             new workbox.expiration.Plugin({
