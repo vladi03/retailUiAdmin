24dc11dfaca7f9675956438e29b497e4c93f9585
vladi <vladi03@hotmail.com>
2021-04-25 22:43:27 -0400

    fix deployment for image

--- nwb.prod.config.js
+++ nwb.prod.config.js
@@ -6,6 +6,7 @@ module.exports = {
         copy: [
             // Copy directory contents to output
             {from: './src/retail-shop-flat-icon_small.png'},
+            {from: './src/retail-shop-flat-icon_small.jpg'},
             {from: './src/retail-shop-flat-icon.jpg'},
             {from: './src/manifest.json'},
             {from: './src/sw.js'}
